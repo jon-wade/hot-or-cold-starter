fa53a5c6f6fb2d4425289d440dc85c96353ab2c3
jon-wade <jonwadeuk@gmail.com>
2016-04-04 15:31:51 +0100

    add in guess counter and new game counter reset

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
+var guesses=0;
 
 $(document).ready(function(){
 	//start a new game when the page loads
+
     newGame();
+    getInput();
 
 
 
-
-
+	//event listeners
+	//new game
+    $('a.new').click(function(){
+        console.log('new game clicked');
+        newGame();
+    });
 
 
 
@@ -26,11 +33,36 @@ $(document).ready(function(){
 function newGame() {
     //generate random number between 1 and 100
     var num = generateNumber();
-    console.log(num);
+    console.log("Random number: " + num);
+    //clear the input box
+    $('#userGuess').val("");
+    //clear the previous guesses
+    $('ul#guessList li').remove();
+    //reset feedback message
+    $('#feedback').text("Make Your Guess!"); //placeholder message
+    //reset guesses variable
+    guesses = 0;
+    //output guess number to page
+    $('#count').text(guesses);
 }
 
 function generateNumber() {
     return Math.floor((Math.random() * 100) +1);
+}
+
+function getInput() {
+    $('#guessButton').click(function(){
+        var input = $('#userGuess').val();
+        console.log(input);
+        //provide feedback
+        $('#feedback').text("Feedback goes here"); //placeholder message
+        //insert previous guesses into list
+        $('ul#guessList').append("<li>" + input + "</li>");
+        //increment guesses
+        guesses++;
+        //output guess number to page
+        $('#count').text(guesses);
+    });
 }
 
 
